2963616e367831112bdfe4bc1dbcf0e7ead526dc
nadia-ctrl-svg <mesultana7@gmail.com>
2025-10-27 16:56:47 +0600

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+    long long X, Y;
+
+    std::cin >> X >> Y;
+
+    std::cout << X << " + " << Y << " = " << (X + Y) << std::endl;
+    std::cout << X << " * " << Y << " = " << (X * Y) << std::endl;
+    std::cout << X << " - " << Y << " = " << (X - Y) << std::endl;
+
+    return 0;
+}

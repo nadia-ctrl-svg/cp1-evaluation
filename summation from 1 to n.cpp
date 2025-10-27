9b92ee049b3581b9037fa3e6f0f6e34a08357602
nadia-ctrl-svg <mesultana7@gmail.com>
2025-10-27 16:58:00 +0600

    Add files via upload

---
+++ summation from 1 to n.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+    long long N;
+
+    std::cin >> N;
+
+    long long summation = N * (N + 1) / 2;
+
+    std::cout << summation << std::endl;
+
+    return 0;
+}
